0aa705dfa70ee4c915a469af7bab523f802aab24
Yuan Huang <huangyuan3h@gmail.com>
2024-06-03 17:24:48 +0800

    update the permission

--- sst.config.ts
+++ sst.config.ts
@@ -1,8 +1,10 @@
 import { SSTConfig } from "sst";
 import getApi from "./cdk/router";
 import { getTableConfig } from "./cdk/table";
-import {  Bucket } from 'sst/constructs';
+import {  Bucket,  } from 'sst/constructs';
+import * as s3 from "aws-cdk-lib/aws-s3";
 
+const bucketArn = "arn:aws:s3:::dev-north-path-site-publicbucket5c3dbab0-e3wdaigzrpyo";
 
 export default {
   config(_input) {
@@ -24,7 +26,12 @@ export default {
 
       api.attachPermissions([authTable, userTable, postTable]);
       const bucket = new Bucket(stack, 'avatar');
-      api.bind([bucket])
+      const bucketImage = new Bucket(stack, "Bucket", {
+        cdk: {
+          bucket: s3.Bucket.fromBucketArn(stack, "IBucket", bucketArn),
+        },
+      });
+      api.bind([bucket, bucketImage])
 
       stack.addOutputs({
         ApiEndpoint: api.url,
